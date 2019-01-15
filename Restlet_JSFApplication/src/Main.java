4f6e0255e8fe326b884caef5e9ca55acd349f447
Michael Hinterdorfer <michael.hinterdorfer@outlook.at>
2019-01-15 08:52:57 +0100

    JSF Application inital commit with generated classes

---
+++ Restlet_JSFApplication/src/Main.java
@@ -0,0 +1,19 @@
+import org.restlet.ext.odata.Generator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//generateFromService();
+	}
+	
+	private static void generateFromService() {
+		Generator generator = new Generator("http://localhost:50021/WoMoService.svc/");
+        
+        try {
+            generator.generate("./src/");
+        }catch(Exception x) {
+            System.out.println(x);
+        }
+	}
+
+}
