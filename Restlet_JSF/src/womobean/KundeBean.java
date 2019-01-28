cd4dfa320454ff4ac6a966614013b6d4cd8adeff
Michael Hinterdorfer <michael.hinterdorfer@outlook.at>
2019-01-28 21:32:15 +0100

    edit kunde

--- Restlet_JSF/src/womobean/KundeBean.java
+++ Restlet_JSF/src/womobean/KundeBean.java
@@ -1,58 +1,101 @@
 package womobean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.annotation.ManagedBean;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.event.RowEditEvent;
+
 import womodao.KundeDAO;
-import womomodel.Fahrzeug;
-import womomodel.Fahrzeug_in_saison;
 import womomodel.Kunde;
 
 @Named
 @SessionScoped
 public class KundeBean implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+	private List<Kunde> kunden = new ArrayList<>();
+
 	@Inject
 	KundeDAO dao;
-	
-	public List<Kunde> getAll(){
+
+	@PostConstruct
+	public void init() {
+		this.kunden = dao.findAll();
+	}
+
+	public List<Kunde> getAll() {
 		return dao.findAll();
 	}
-	
-	public HashMap<String, Integer> getAllDropdown(){
+
+	public HashMap<String, Integer> getAllDropdown() {
 		HashMap<String, Integer> kunden = new HashMap<String, Integer>();
-		for(Kunde kunde : dao.findAll()) {
+		for (Kunde kunde : dao.findAll()) {
 			kunden.put(kunde.getVorname() + " " + kunde.getNachname(), kunde.getIdKunde());
 		}
-        return kunden;
+		return kunden;
 	}
-	
+
+	public HashMap<String, Integer> getSingleDropdown(int kundenNr) {
+		HashMap<String, Integer> kunden = new HashMap<String, Integer>();
+		kunden.put(dao.getById(kundenNr).getVorname() + " " + dao.getById(kundenNr).getNachname(),
+				dao.getById(kundenNr).getIdKunde());
+		return kunden;
+	}
+
 	public Kunde getById(int id) {
 		return dao.getById(id);
 	}
-	
+
 	public Kunde findByParameters(String vn, String nn, String str, String plz, String ort) {
 		return dao.findByParameters(vn, nn, str, plz, ort);
 	}
-	
+
 	public void add(String vorname, String nachname, String strasse, int plz, String ort) {
 		FacesMessage message = null;
-         
-        if(dao.add(vorname, nachname, strasse, plz, ort)) {
-            message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Kunde hinzugefügt", "Name: " + vorname + " " + nachname);
-        } else {
-            message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Fehler beim Einfügen", "Kunde konnte nicht eingefügt werden.");
-        }
-        FacesContext.getCurrentInstance().addMessage(null, message);
+
+		if (dao.add(vorname, nachname, strasse, plz, ort)) {
+			message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Kunde hinzugefügt",
+					"Name: " + vorname + " " + nachname);
+		} else {
+			message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Fehler beim Einfügen",
+					"Kunde konnte nicht eingefügt werden.");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void onRowEdit(RowEditEvent event) {
+		FacesMessage message = null;
+		Kunde edit = (Kunde) event.getObject();
+		if (dao.update(edit)) {
+			message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Kunde bearbeitet",
+					"Kundennummer: " + edit.getIdKunde());
+		} else {
+			message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Fehler beim Bearbeiten",
+					"Kunde konnte nicht bearbeitet werden.");
+		}
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void onRowCancel(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Bearbeitung abgebrochen",
+				"Kundennummer: " + ((Kunde) event.getObject()).getIdKunde());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public List<Kunde> getKunden() {
+		return kunden;
+	}
+
+	public void setKunden(List<Kunde> kunden) {
+		this.kunden = kunden;
 	}
 
 }
--- Restlet_JSF/src/womodao/KundeDAO.java
+++ Restlet_JSF/src/womodao/KundeDAO.java
@@ -10,39 +10,40 @@ import javax.inject.Inject;
 import org.restlet.ext.odata.Query;
 
 import service.WoMoService;
-import womomodel.Fahrzeug_in_saison;
 import womomodel.Kunde;
 
 @ManagedBean
 @SessionScoped
 public class KundeDAO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Inject 
+
+	@Inject
 	transient private WoMoService service;
-	
-	public List<Kunde> findAll(){
+
+	public List<Kunde> findAll() {
 		Query<Kunde> query = service.createKundeQuery("/Kundes");
 		List<Kunde> kunden = new ArrayList<>();
-		for(Kunde kunde : query) {
+		for (Kunde kunde : query) {
 			kunden.add(kunde);
 		}
 		return kunden;
 	}
-	
+
 	public Kunde getById(int id) {
 		Query<Kunde> query = service.createKundeQuery("/Kundes(" + id + ")");
 		return query.iterator().next();
 	}
-	
+
 	public Kunde findByParameters(String vn, String nn, String str, String plz, String ort) {
 		try {
-			Query<Kunde> query = service.createKundeQuery("/Kundes").filter("((vorname eq '" + vn + "') and (nachname eq '" + nn + "') and (strasse eq '" + str + "') and (plz eq " + plz + ") and (ort eq '" + ort + "'))");
-			for(Kunde kunde : query) {
+			Query<Kunde> query = service.createKundeQuery("/Kundes")
+					.filter("((vorname eq '" + vn + "') and (nachname eq '" + nn + "') and (strasse eq '" + str
+							+ "') and (plz eq " + plz + ") and (ort eq '" + ort + "'))");
+			for (Kunde kunde : query) {
 				return kunde;
 			}
 			return null;
-		}catch(Exception ex) {
+		} catch (Exception ex) {
 			return null;
 		}
 	}
@@ -57,8 +58,18 @@ public class KundeDAO implements Serializable {
 			kunde.setOrt(ort);
 			service.addEntity(kunde);
 			return true;
-		}catch (Exception ex) {
+		} catch (Exception ex) {
 			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	public boolean update(Kunde kunde) {
+		try {
+			service.updateEntity(kunde);
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
 			return false;
 		}
 	}
